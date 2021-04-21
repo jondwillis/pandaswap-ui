002698ff1a90d2a2fdf23a6284902cb454ca814f
Jon Willis <jondwillis@gmail.com>
2021-04-21 09:53:27 -0400

    add bsc wallet connector

--- src/connectors/index.ts
+++ src/connectors/index.ts
@@ -6,6 +6,7 @@ import { PortisConnector } from '@web3-react/portis-connector'
 
 import { FortmaticConnector } from './Fortmatic'
 import { NetworkConnector } from './NetworkConnector'
+import { BscConnector } from '@binance-chain/bsc-connector'
 
 const NETWORK_URL = process.env.REACT_APP_NETWORK_URL
 const FORMATIC_KEY = process.env.REACT_APP_FORTMATIC_KEY
@@ -27,6 +28,10 @@ export function getNetworkLibrary(): Web3Provider {
 }
 
 export const injected = new InjectedConnector({
+  supportedChainIds: [56]
+})
+
+export const bsc = new BscConnector({
   supportedChainIds: [56]
 })
 
--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,7 +1,7 @@
 import { ChainId, JSBI, Percent, Token, WETH } from 'uniswap-bsc-sdk'
 import { AbstractConnector } from '@web3-react/abstract-connector'
 
-import { injected, walletconnect } from '../connectors'
+import { bsc, injected, walletconnect } from '../connectors'
 
 export const ROUTER_ADDRESS = '0x29D1Adbb65d93a5710cafe2EF0E8131f64E6AB22'
 
@@ -87,6 +87,14 @@ export const SUPPORTED_WALLETS: { [key: string]: WalletInfo } = {
     href: null,
     color: '#4196FC',
     mobile: true
+  },
+  BSC: {
+    connector: bsc,
+    name: 'Binance Chain Wallet',
+    iconName: 'binanceChainWallet.svg',
+    description: 'Connect to Binance Chain Wallet',
+    href: null,
+    color: '#F0B90B'
   }
 }
 
