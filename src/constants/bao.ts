38172d4e65f4cbb6bde8ad0316b1cae902813efa
Jon Willis <jondwillis@gmail.com>
2021-05-07 10:17:16 -0700

    fix rhino-bnb cake AGAIN

--- src/constants/bao.ts
+++ src/constants/bao.ts
@@ -331,7 +331,7 @@ export const supportedPools: SupportedPool[] = [
     tokenAddresses: {
       56: [tokenMap.RHINO, tokenMap.BNB],
     },
-    tokenDecimals: 18,
+    tokenDecimals: 9,
     name: 'Rhino Remoulade',
     symbol: 'RHINO-BNB PNDA-V2',
     tokenSymbol: 'RHINO',
@@ -346,8 +346,8 @@ export const supportedPools: SupportedPool[] = [
     tokenAddresses: {
       56: [tokenMap.RHINO, tokenMap.BNB],
     },
-    tokenDecimals: 9,
-    name: 'Rhino Runbles',
+    tokenDecimals: 18,
+    name: 'Rhino Rumbles',
     symbol: 'RHINO-BNB CAKELP',
     tokenSymbol: 'RHINO',
     poolType: PoolType.CAKE,
